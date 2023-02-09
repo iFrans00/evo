eb557eb92a1e92827867f9b47d03dc274351dbab
iFrans00 <fransa0889@gmail.com>
2023-02-09 14:31:20 +0700

    Updates

---
+++ assets/Home.c1cfe9a3.js
@@ -0,0 +1,1 @@
+import{_ as e,r as t,o as n,f as r}from"./index.d70ef015.js";const s={mounted(){}};function c(a,_,l,p,d,f){const o=t("HelloWorld");return n(),r(o)}const u=e(s,[["render",c]]);export{u as default};
