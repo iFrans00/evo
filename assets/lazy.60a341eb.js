aa87eede4c2946ee11a56fa8592bafd02d55eeb9
iFrans00 <fransa0889@gmail.com>
2023-02-09 10:16:39 +0700

    Updates

---
+++ assets/lazy.60a341eb.js
@@ -0,0 +1,1 @@
+import{p as r,C as n,l as u,G as l}from"./index.7c024e72.js";const c=r({eager:Boolean},"lazy");function m(a,o){const e=n(!1),s=u(()=>e.value||a.eager||o.value);l(o,()=>e.value=!0);function t(){a.eager||(e.value=!1)}return{isBooted:e,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
